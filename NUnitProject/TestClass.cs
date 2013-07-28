c17ef380132e84a2c1e68942c4047ee57e281375
JohnnyPP <johnnnyppp@gmail.com>
2013-07-28 14:55:25 +0200

    Initial release
    http://www.youtube.com/watch?v=1TPZetHaZ-A

---
+++ NUnitProject/TestClass.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace NUnit
+{
+    [TestFixture]
+    public class TestClass
+    {
+        [Test]
+        public void TestPositive()
+        {
+            int x = 10;
+            int y = 10;
+
+            Assert.AreEqual(x, y);
+        }
+
+        [Test]
+        public void TestNegative()
+        {
+            if (true)
+            {
+                Assert.Fail("This is a failure");
+                //Assert.Pass("This will pass");
+            }
+        }
+
+        [Test,ExpectedException(typeof(NotSupportedException))]     //this will pass the test
+        public void ExpectedExceptionTest()
+        {
+            throw new NotSupportedException();
+        }
+
+        [Test,Ignore]                                               //this will be ignored
+        public void NotImplementedException()
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
